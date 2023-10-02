c1a984ab7b11a4e94ba014be1ffabdb9fd436c74
Jacob Jerrell <jacob@jjerrell.dev>
2023-10-01 22:55:56 -0500

    Update logging implementation
    - Adds LoggingPageTree for the logging demo
      - Lower-to-low level logging implementation
    - Update LoggingPageViewModel to plant LoggingPageTree
      - Uproots during ViewModel.onClear()
    - Refactor log usage within LoggingPage relatives

--- app/src/main/java/dev/jjerrell/android/playground/ui/tagging/PlaygroundTag.kt
+++ app/src/main/java/dev/jjerrell/android/playground/ui/tagging/PlaygroundTag.kt
@@ -1,3 +1,4 @@
+/* (C) 2023 Jacob Jerrell */
 package dev.jjerrell.android.playground.ui.tagging
 
 object PlaygroundTag {
@@ -13,13 +14,14 @@ object PlaygroundTag {
 
             const val PageHeader = "LOGGING_PAGE_HEADER"
             val LogLevelHeader = { buttonName: String -> "LOG_LEVEL_HEADER_$buttonName" }
-            val CustomLogLevelButton = { index: Int -> "LOGGING_BUTTON_CUSTOM_$index"}
+            val CustomLogLevelButton = { index: Int -> "LOGGING_BUTTON_CUSTOM_$index" }
             val LogLevelButton = { logLevel: Int, success: Boolean? ->
-                val buttonTagName = when (success) {
-                    true -> SUCCESS_BUTTON
-                    false -> FAILURE_BUTTON
-                    else -> WARNING_BUTTON
-                }
+                val buttonTagName =
+                    when (success) {
+                        true -> SUCCESS_BUTTON
+                        false -> FAILURE_BUTTON
+                        else -> WARNING_BUTTON
+                    }
                 "${buttonTagName}_LOGGING_BUTTON_${logLevel}"
             }
         }
