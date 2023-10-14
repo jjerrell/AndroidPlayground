b8f301ae01586ce695c6457c7e619330e31b7859
Jacob Jerrell <jacob@jjerrell.dev>
2023-10-13 19:52:36 -0500

    Finalizes Koin configuration at the app level. Includes a Logger implementation which uses Timber for consistency

--- app/src/main/java/dev/jjerrell/android/playground/PlaygroundApplication.kt
+++ app/src/main/java/dev/jjerrell/android/playground/PlaygroundApplication.kt
@@ -2,6 +2,13 @@
 package dev.jjerrell.android.playground
 
 import android.app.Application
+import dev.jjerrell.android.playground.feature.about.aboutModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.KoinApplication
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.logger.Level
+import org.koin.core.logger.Logger
+import org.koin.core.logger.MESSAGE
 import timber.log.Timber
 
 class PlaygroundApplication : Application() {
@@ -12,6 +19,28 @@ class PlaygroundApplication : Application() {
         } else {
             Timber.plant(CrashReportingTree())
         }
+
+        startKoin {
+            setupLogging()
+            androidContext(this@PlaygroundApplication)
+            modules(aboutModule())
+        }
+    }
+
+    private fun KoinApplication.setupLogging() {
+        val koinLogger =
+            object : Logger() {
+                override fun display(level: Level, msg: MESSAGE) {
+                    when (level) {
+                        Level.DEBUG -> Timber.d(msg)
+                        Level.INFO -> Timber.i(msg)
+                        Level.WARNING -> Timber.w(msg)
+                        Level.ERROR -> Timber.e(msg)
+                        Level.NONE -> Timber.wtf(msg)
+                    }
+                }
+            }
+        logger(koinLogger)
     }
 }
 
