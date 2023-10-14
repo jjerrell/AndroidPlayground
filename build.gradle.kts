a7a729e1c764096922860fa600047d7aab51d6fa
Jacob Jerrell <jacob@jjerrell.dev>
2023-10-13 22:53:11 -0500

    Add and configure Dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,39 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.kotlin) apply false
     alias(libs.plugins.diffplug.spotless)
+    id("org.jetbrains.dokka") version "1.9.0"
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
+
+    // configure all format tasks at once
+    tasks.withType<org.jetbrains.dokka.gradle.DokkaTaskPartial>().configureEach {
+
+        moduleName.set(project.name)
+        moduleVersion.set(project.version.toString())
+        failOnWarning.set(false)
+        suppressObviousFunctions.set(true)
+        suppressInheritedMembers.set(true)
+        offlineMode.set(false)
+
+        dokkaSourceSets.configureEach {
+            suppress.set(false)
+            displayName.set(name)
+            documentedVisibilities.set(
+                setOf(
+                    org.jetbrains.dokka.DokkaConfiguration.Visibility.PUBLIC,
+                    org.jetbrains.dokka.DokkaConfiguration.Visibility.PRIVATE,
+                    org.jetbrains.dokka.DokkaConfiguration.Visibility.INTERNAL,
+                )
+            )
+            reportUndocumented.set(true)
+            skipEmptyPackages.set(true)
+            skipDeprecated.set(false)
+            suppressGeneratedFiles.set(true)
+            jdkVersion.set(17)
+        }
+    }
 }
 
 configure<com.diffplug.gradle.spotless.SpotlessExtension> {
